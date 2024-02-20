fb7a746c247e32fd88c7db3bb46d661722cdd8c4
LuNa <60292888+weeb-deutch@users.noreply.github.com>
2024-02-20 18:36:17 +0000

    IT WORKS

--- main.ts
+++ main.ts
@@ -1,1 +1,37 @@
-
+input.onButtonPressed(Button.A, function () {
+    logging = true
+    serial.writeLine("start")
+})
+input.onButtonPressed(Button.AB, function () {
+    serial.writeLine("delete")
+    basic.showIcon(IconNames.No)
+    datalogger.deleteLog(datalogger.DeleteType.Full)
+    basic.showString("deleted")
+})
+input.onButtonPressed(Button.B, function () {
+    logging = false
+    serial.writeLine("end")
+})
+let logging = false
+datalogger.setColumnTitles(
+"Light",
+"Temp"
+)
+loops.everyInterval(1000, function () {
+    if (logging) {
+        basic.showIcon(IconNames.Yes)
+        datalogger.log(
+        datalogger.createCV("Light", input.lightLevel()),
+        datalogger.createCV("Temp", input.temperature())
+        )
+        datalogger.includeTimestamp(FlashLogTimeStampFormat.Seconds)
+    }
+})
+basic.forever(function () {
+    serial.redirect(
+    SerialPin.USB_TX,
+    SerialPin.USB_RX,
+    BaudRate.BaudRate115200
+    )
+    serial.redirectToUSB()
+})
