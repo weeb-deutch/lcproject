1131fbfbaa3c8743a766e987e7dd4432b46885c5
LuNa <60292888+weeb-deutch@users.noreply.github.com>
2024-03-13 11:37:40 +0000

    i wanna kms
    log stuffffffffffffffff

--- PLEASE.py
+++ PLEASE.py
@@ -1,7 +1,14 @@
 import serial
+import log
+
 
 connection = serial.Serial("COM3")
 while True:
+    log.set_labels('temperature', 'sound', 'light')
+    log.add({
+      'temperature': temperature(),
+      'light': display.read_light_level()
+    })
     print("Light and temp monitor")
     data = connection.readline().decode().strip()
     while data != "start" :
